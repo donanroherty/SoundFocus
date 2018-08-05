faf7f0cf48446d9744e372d99f0ee3c0a8618a56
Ronan Doherty <donanroherty@gmail.com>
2018-08-05 12:34:11 +0100

    Made Clock component functional and moved logic to TimerScreen

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -2,71 +2,12 @@ import React, { Component } from 'react'
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import Icon from 'react-native-vector-icons/Ionicons'
 
-const defaultProps = {
-  intervalTime: 1500 /* 1500 = 25mins*/
-}
-
-class Clock extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      remaining: this.props.intervalTime
-    }
-
-    this.startTime = 0
-    this.endTime = 0
-    this.paused = true
-    this.timer = null
+const Clock = props => {
+  handleToggleTimerActive = () => {
+    props.toggleTimerActive()
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.remaining == 0) {
-      this.onTimerComplete()
-    }
-  }
-
-  resume = () => {
-    console.log('resumeClock')
-    this.startTime = new Date().getTime() * 0.001 // Get time in seconds
-    this.endTime = this.startTime + this.props.intervalTime
-    this.setState({ remaining: this.endTime - this.startTime })
-
-    this.paused = false
-
-    this.timer = setInterval(this.tick, 1000)
-  }
-
-  pause = () => {
-    console.log('pauseClock')
-    clearInterval(this.timer)
-    this.paused = true
-  }
-
-  tick = () => {
-    this.setState(state => {
-      if (state.remaining - 1 <= 0) {
-        this.onTimerComplete()
-      }
-      return { remaining: state.remaining - 1 }
-    })
-  }
-
-  onTimerComplete = () => {
-    console.log('timerComplete')
-    clearInterval(this.timer)
-    this.paused = true
-  }
-
-  handleTimerPress = () => {
-    if (this.paused) {
-      this.resume()
-    } else {
-      this.pause()
-    }
-  }
-
-  formatSeconds = inSeconds => {
+  const formatSeconds = inSeconds => {
     const mins = Math.floor(inSeconds / 60)
     const secs = inSeconds % 60
 
@@ -76,91 +17,44 @@ class Clock extends Component {
     }
   }
 
-  handleResetTimer = () => {
-    console.log('timer reset')
-    if (this.timer) {
-      clearInterval(this.timer)
-      this.paused = true
-      this.setState({
-        remaining: this.props.intervalTime
-      })
-    }
-  }
+  const remaining = formatSeconds(props.remaining)
+  const underAMinuteLeft = Math.floor(props.remaining / 60) <= 0
 
-  render() {
-    const remaining = this.formatSeconds(this.state.remaining)
+  const content = underAMinuteLeft ? (
+    // Hide minutes and colon if less than 1 minute remains on the clock
+    <View style={styles.clockContainer}>
+      <Text style={[styles.baseText, styles.seconds]}>{remaining.seconds}</Text>
+    </View>
+  ) : (
+    <View style={styles.clockContainer}>
+      <Text style={[styles.baseText, styles.minutes]}>{remaining.minutes}</Text>
+      <Text style={[styles.baseText, styles.colon]}>:</Text>
+      <Text style={[styles.baseText, styles.seconds]}>{remaining.seconds}</Text>
+    </View>
+  )
 
-    const underAMinuteLeft = Math.floor(this.state.remaining / 60) <= 0
-
-    return (
-      <View style={styles.wrapper}>
-        <TouchableOpacity onPress={this.handleResetTimer}>
-          <Icon name="md-refresh" size={30} /*color="black"*/ />
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.clockContainer} onPress={this.handleTimerPress}>
-          {underAMinuteLeft ? (
-            <View />
-          ) : (
-            <View style={styles.minutesContainer}>
-              <Text style={styles.baseText}>{remaining.minutes}</Text>
-            </View>
-          )}
-
-          <View />
-
-          {underAMinuteLeft ? (
-            <View />
-          ) : (
-            <View style={styles.colonContainer}>
-              <Text style={styles.baseText}>:</Text>
-            </View>
-          )}
-
-          <View
-            style={
-              underAMinuteLeft
-                ? [styles.secondsContainer, { justifyContent: 'center', alignItems: 'center' }]
-                : styles.secondsContainer
-            }
-          >
-            <Text style={styles.baseText}>{remaining.seconds}</Text>
-          </View>
-        </TouchableOpacity>
-      </View>
-    )
-  }
+  return (
+    <View>
+      <TouchableOpacity style={styles.clockContainer} onPress={handleToggleTimerActive}>
+        {content}
+      </TouchableOpacity>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-  wrapper: {
-    flexDirection: 'column',
-    alignItems: 'center'
-  },
   clockContainer: {
-    // flex: 1,
     flexDirection: 'row'
-    // justifyContent: 'center'
-    // alignItems: 'center'
   },
-  minutesContainer: {
-    // width: 150,
-    // height: 150,
-    // backgroundColor: 'powderblue',
+  minutes: {
     justifyContent: 'center',
     alignItems: 'flex-end'
   },
-  secondsContainer: {
-    // width: 150,
-    // height: 150,
-    // backgroundColor: 'powderblue',
+  seconds: {
     justifyContent: 'center',
     alignItems: 'flex-start'
   },
-  colonContainer: {
-    // width: 30,
-    // height: 150,
-    // backgroundColor: 'skyblue',
+  colon: {
     justifyContent: 'center',
     alignItems: 'center'
   },
@@ -169,7 +63,5 @@ const styles = StyleSheet.create({
     fontWeight: 'normal'
   }
 })
-
-Clock.defaultProps = defaultProps
 
 export default Clock
--- src/screens/Timer.js
+++ src/screens/Timer.js
@@ -3,11 +3,90 @@ import { Text, View, StyleSheet, TouchableOpacity } from 'react-native'
 import Icon from 'react-native-vector-icons/Ionicons'
 import Clock from 'components/Clock'
 
+const defaultProps = {
+  intervalTime: 5 /* 1500 = 25mins*/
+}
 class Timer extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      remaining: this.props.intervalTime,
+      timerIsActive: false
+    }
+
+    this.timer = null
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.remaining > 0 && this.state.remaining === 0) {
+      this.onTimerComplete()
+    }
+  }
+
+  toggleTimerActive = () => {
+    if (this.state.timerIsActive) {
+      this.stopTimer()
+    } else {
+      this.startTimer()
+    }
+  }
+
+  startTimer = () => {
+    if (this.state.timerIsActive) {
+      return
+    }
+
+    console.log('startTimer')
+
+    this.setState({
+      timerIsActive: true
+    })
+
+    this.timer = setInterval(this.tick, 1000)
+  }
+
+  stopTimer = () => {
+    if (!this.state.timerIsActive) {
+      return
+    }
+
+    console.log('stopTimer')
+    clearInterval(this.timer)
+    this.setState({ timerIsActive: false })
+  }
+
+  tick = () => {
+    this.setState(state => {
+      return { remaining: state.remaining - 1 }
+    })
+  }
+
+  handleResetTimer = () => {
+    console.log('timer reset')
+    if (this.timer) {
+      clearInterval(this.timer)
+
+      this.setState({
+        remaining: this.props.intervalTime,
+        timerIsActive: false
+      })
+    }
+  }
+
+  onTimerComplete = () => {
+    console.log('timerComplete')
+    clearInterval(this.timer)
+    this.setState({ timerIsActive: false })
+  }
+
   render() {
     return (
       <View style={styles.wrapper}>
-        <Clock />
+        <TouchableOpacity onPress={this.handleResetTimer}>
+          <Icon name="md-refresh" size={30} />
+        </TouchableOpacity>
+        <Clock remaining={this.state.remaining} toggleTimerActive={this.toggleTimerActive} />
       </View>
     )
   }
@@ -22,5 +101,7 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   }
 })
+
+Timer.defaultProps = defaultProps
 
 export default Timer
