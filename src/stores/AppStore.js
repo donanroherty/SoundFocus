78e2096c30bb01d84c63e8f781761daaed52d803
Ronan Doherty <donanroherty@gmail.com>
2018-08-28 14:18:00 +0100

    Set up local data persistance with AsyncStorage

--- src/stores/AppStore.js
+++ src/stores/AppStore.js
@@ -1,9 +1,27 @@
+import { observable } from 'mobx'
 import UserPropertyStore from 'stores/UserPropertyStore'
 import TimerStore from 'stores/TimerStore'
+import LocalStorageHandler from 'stores/utils/LocalStorageHandler'
 
 export default class AppStore {
   constructor() {
     this.userPropertyStore = new UserPropertyStore(this)
     this.timerStore = new TimerStore(this)
+
+    this.localStorageHandler = new LocalStorageHandler()
+    this.localStorageHandler.trackStore(this.userPropertyStore)
+
+    // Load user settings
+    this.localStorageHandler
+      .refreshStore(this.userPropertyStore)
+      .then(() => {
+        this.userDataLoaded = true
+      })
+      .catch(e => {
+        console.log(e)
+      })
   }
+
+  @observable
+  userDataLoaded = false
 }
