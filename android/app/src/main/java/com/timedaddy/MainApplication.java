8b11d646d5f20bac13c334344f826c5fdaf39879
Ronan Doherty <donanroherty@gmail.com>
2018-08-29 16:05:18 +0100

    Fixed react-native-sound linking

--- android/app/src/main/java/com/timedaddy/MainApplication.java
+++ android/app/src/main/java/com/timedaddy/MainApplication.java
@@ -25,8 +25,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNSoundPackage(),
-            new VectorIconsPackage()
+          new RNSoundPackage(),
+          new VectorIconsPackage()
       );
     }
 
