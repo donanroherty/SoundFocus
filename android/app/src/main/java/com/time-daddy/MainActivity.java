935f7ac9ed1523fd61c6a313d21c6b85b18b5137
Ronan Doherty <donanroherty@gmail.com>
2018-09-01 13:42:13 +0100

    Regenerated android folder and upgraded node_modules

--- android/app/src/main/java/com/time-daddy/MainActivity.java
+++ android/app/src/main/java/com/time-daddy/MainActivity.java
@@ -1,4 +1,4 @@
-package com.timedaddy;
+package com.time-daddy;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "timedaddy";
+        return "time-daddy";
     }
 }
--- android/app/src/main/java/com/time-daddy/MainApplication.java
+++ android/app/src/main/java/com/time-daddy/MainApplication.java
@@ -1,9 +1,8 @@
-package com.timedaddy;
+package com.time-daddy;
 
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -25,7 +24,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-          new VectorIconsPackage()
+            new VectorIconsPackage()
       );
     }
 
