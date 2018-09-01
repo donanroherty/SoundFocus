a21acebc557e630697d9a7ead6a17bc5413e64bc
Ronan Doherty <donanroherty@gmail.com>
2018-09-01 16:02:37 +0100

    Implemented react-native-video to handle audio.  Tracks looping nicely

--- src/components/AudioTrack.js
+++ src/components/AudioTrack.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { View, Text, Button } from 'react-native'
-// import Video from 'react-native-video'
+import Video from 'react-native-video'
 
 const defaultProps = {
   name: 'Track Name',
@@ -10,18 +10,67 @@ const defaultProps = {
 class AudioTrack extends Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      playing: false,
+      duration: 0,
+      currentTime: 0
+    }
 
-    this.sound = null
+    this.player = null
   }
 
-  handlePlaySound = () => {}
+  handlePlaySound = () => {
+    this.setState((prevState, props) => ({ playing: !prevState.playing }))
+  }
+
+  onLoadStart = () => {
+    console.log('loading')
+  }
+  onLoad = data => {
+    console.log('loaded ', data)
+    this.setState({ duration: data.duration })
+  }
+  onProgress = data => {
+    this.setState({ currentTime: data.currentTime })
+  }
+  onEnd = () => {
+    console.log('onEnd ')
+  }
+  onError = err => {
+    console.log('onError ', err)
+  }
 
   render() {
     return (
       <View>
-        <Text> {this.props.name} </Text>
-        <Button title="Play Sound" onPress={this.handlePlaySound} />
+        <Text>{this.props.name}</Text>
+        <Text>Time: {this.state.currentTime}</Text>
+        <Button
+          title={this.state.playing ? `Pause ${this.props.name}` : `Play ${this.props.name}`}
+          onPress={this.handlePlaySound}
+        />
+
+        {this.state.playing && (
+          <Video
+            source={this.props.audioAsset}
+            ref={ref => {
+              this.player = ref
+            }}
+            rate={1.0}
+            volume={1.0}
+            muted={false}
+            paused={false}
+            resizeMode="cover"
+            repeat={true}
+            playInBackground={true}
+            playWhenInactive={false}
+            onLoadStart={this.onLoadStart}
+            onLoad={this.onLoad}
+            onProgress={this.onProgress}
+            onEnd={this.onEnd}
+            onError={this.onError}
+          />
+        )}
       </View>
     )
   }
