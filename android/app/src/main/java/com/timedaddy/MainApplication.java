1c17e3de325d543fbbe9290b9e95b274d1b9dc9b
Ronan Doherty <donanroherty@gmail.com>
2018-09-02 21:15:37 +0100

    Added react-native-background-timer and replaced interval code to run in background

--- android/app/src/main/java/com/timedaddy/MainApplication.java
+++ android/app/src/main/java/com/timedaddy/MainApplication.java
@@ -3,6 +3,7 @@ package com.timedaddy;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.ocetnik.timer.BackgroundTimerPackage;
 import com.brentvatne.react.ReactVideoPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new BackgroundTimerPackage(),
             new ReactVideoPackage(),
             new VectorIconsPackage()
       );
