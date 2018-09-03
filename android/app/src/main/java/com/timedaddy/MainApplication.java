000f48d2131e7f4f100fb78e91e3dbb7a5ddb482
Ronan Doherty <donanroherty@gmail.com>
2018-09-03 09:34:28 +0100

    Added react-native-push-notification to project

--- android/app/src/main/java/com/timedaddy/MainApplication.java
+++ android/app/src/main/java/com/timedaddy/MainApplication.java
@@ -3,6 +3,7 @@ package com.timedaddy;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.ocetnik.timer.BackgroundTimerPackage;
 import com.brentvatne.react.ReactVideoPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -13,6 +14,7 @@ import com.facebook.soloader.SoLoader;
 
 import java.util.Arrays;
 import java.util.List;
+
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -26,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativePushNotificationPackage(),
             new BackgroundTimerPackage(),
             new ReactVideoPackage(),
             new VectorIconsPackage()
