8d91b043fd4a3129d3cdf0f87c229af57ccc28cc
Ronan Doherty <donanroherty@gmail.com>
2018-09-19 16:56:40 +0100

    Styled user property modal

--- src/components/UserPropertyModal.js
+++ src/components/UserPropertyModal.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet, TextInput, Button } from 'react-native'
+import { View, Text, StyleSheet, TextInput, Button, TouchableOpacity } from 'react-native'
 import Modal from 'react-native-modal'
+import Icon from 'react-native-vector-icons/Ionicons'
+import Theme from 'theme'
 
 const defaultProps = {
   shortName: 'mySetting',
@@ -40,25 +42,53 @@ export default class UserPropertyModal extends Component {
         onBackdropPress={this.props.closeModal}
         onBackButtonPress={this.props.closeModal}
         avoidKeyboard={false}
+        style={styles.wrapper}
       >
-        <View style={styles.modal}>
-          <Text style={styles.headingText}>{this.props.name}</Text>
+        <View style={styles.settingBox}>
+          {/* Top Row */}
+          <View style={styles.topRow}>
+            <View style={styles.topRowLeftCol} />
 
-          <TextInput
-            keyboardType="numeric"
-            placeholder={`${this.props.value}`}
-            maxLength={3}
-            selectTextOnFocus={true}
-            autoFocus={true}
-            value={this.state.value}
-            onChangeText={this.handleInput}
-            clearTextOnFocus={true}
-            style={styles.inputField}
-          />
+            <View style={styles.topRowMidCol}>
+              <Text style={styles.headingText}>{this.props.name}</Text>
+            </View>
 
-          <View style={styles.buttonContainer}>
-            <Button title="cancel" onPress={this.props.closeModal} style={styles.button} />
-            <Button title="ok" onPress={this.handleSubmit} style={styles.button} />
+            <TouchableOpacity onPress={this.props.closeModal} style={styles.topRowRightCol}>
+              <Icon
+                name="ios-close"
+                size={45}
+                color={Theme.colorPrimary}
+                style={styles.closeIcon}
+              />
+            </TouchableOpacity>
+          </View>
+
+          {/* Middle Row */}
+          <View style={styles.middleRow}>
+            <View style={styles.middleRowTopRow}>
+              <TextInput
+                keyboardType="numeric"
+                placeholder={`${this.props.value}`}
+                maxLength={3}
+                selectTextOnFocus={true}
+                autoFocus={true}
+                value={this.state.value}
+                onChangeText={this.handleInput}
+                clearTextOnFocus={true}
+                style={styles.inputField}
+              />
+            </View>
+
+            <View style={styles.middleRowBottomRow}>
+              <Text>_________________________</Text>
+            </View>
+          </View>
+
+          {/* Bottom Row */}
+          <View style={styles.bottomRow}>
+            <TouchableOpacity onPress={this.handleSubmit} style={styles.okButtonContainer}>
+              <Text style={styles.okButton}>OK</Text>
+            </TouchableOpacity>
           </View>
         </View>
       </Modal>
@@ -68,24 +98,83 @@ export default class UserPropertyModal extends Component {
 
 const styles = StyleSheet.create({
   wrapper: {
-    flex: 1
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    marginTop: '30%'
   },
-  modal: {
+  settingBox: {
     backgroundColor: 'white',
+    alignItems: 'center',
+    width: '85%',
+    borderRadius: 10,
+    height: 190
+  },
+
+  topRow: {
+    flex: 1,
+    flexDirection: 'row',
+    width: '100%'
+    // backgroundColor: 'cyan'
+  },
+  middleRow: {
+    // flex: 1,
+    flexGrow: 1,
+    flexDirection: 'column',
+    flexDirection: 'column',
+    width: '100%',
+    justifyContent: 'center'
+    // backgroundColor: 'grey'
+  },
+  bottomRow: {
+    flex: 1,
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'center'
+    // backgroundColor: 'cyan'
+  },
+
+  topRowLeftCol: {
+    width: 40,
+    height: 40
+  },
+  topRowMidCol: { flex: 1, flexDirection: 'row', justifyContent: 'center' },
+
+  // close button container
+  topRowRightCol: {
+    flexDirection: 'row',
     justifyContent: 'center',
-    alignItems: 'center'
+    width: 40,
+    height: 40
+    // backgroundColor: 'red'
   },
+
+  middleRowTopRow: { flexDirection: 'row', justifyContent: 'center' },
+  middleRowBottomRow: { flexDirection: 'row', justifyContent: 'center', marginTop: -20 },
+
   headingText: {
-    fontSize: 24
+    fontSize: 24,
+    marginTop: 10
   },
+  closeIcon: {
+    marginTop: 3
+  },
+
   inputField: {
     fontSize: 22
   },
-  buttonContainer: {
-    flexDirection: 'row'
+
+  okButtonContainer: {
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    width: 50,
+    alignItems: 'center'
   },
-  button: {
-    margin: 5
+
+  okButton: {
+    fontFamily: Theme.font.regular,
+    fontSize: 20,
+    color: Theme.colorPrimary
   }
 })
 
