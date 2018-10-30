01fcfd5612e6f091255292f050594507fd45a8b0
Ronan Doherty <donanroherty@gmail.com>
2018-10-30 17:29:25 +0000

    Added launcher icon

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,11 +1,5 @@
 import Fonts from 'theme/fonts'
 
-export const rgba = (r = 0, g = 0, b = 0, a = 0) => {
-  return `rgba(${r},${g},${b},${a})`
-}
-
-const brandColor = rgba(38, 153, 251, 1)
-
 export const getBackgroundColor = isDarkMode => {
   return isDarkMode ? theme.colorBackgroundDarkMode : theme.colorBackground
 }
@@ -15,15 +9,15 @@ export const getTextColor = isDarkMode => {
 }
 
 const theme = {
-  colorText: '#4D4D4D',
+  colorText: '#2196F3',
+  colorTextDarkMode: '#FFFFFF',
+
   colorPrimary: '#2699FB',
   colorPrimaryLight: '#B0D8FB',
-  colorPrimaryLighter: '#E2EFFB',
 
   black: '#000000',
-  colorBackground: '#FFFFFF',
 
-  colorTextDarkMode: '#FFFFFF',
+  colorBackground: '#FFFFFF',
   colorBackgroundDarkMode: '#000000',
 
   font: Fonts.roboto,
